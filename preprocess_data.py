4e18d1ca49b8ebb4fb956ab7c1c4ff4279e4c57f
just-passerby <35185525+just-passerby@users.noreply.github.com>
2019-08-05 18:01:34 +0800

    Add files via upload

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
+from scipy.stats import pearsonr
 
 
 #获取文件地址
@@ -70,7 +71,7 @@ def feature_project (data,df,name,k):
         df[name + '时间占比'] = data.sum()/len(data)
     
     #温度信号取其均值与标准差为特征
-    elif name == '温度信号' or name == '流量信号':
+    elif name == '温度信号' :
         df[name + '均值'] = data.mean()
         df[name + '标准差'] = data.std()
     
@@ -166,21 +167,28 @@ def coupled_feature (dataframe,df):
     
     #取出列名表
     column_list = dataframe.columns.values.tolist()
+
     #循环将特征两两相乘组合
-    for i in range (3,13):
-        for j in range (i+1,13):
-            mutiple = dataframe.iloc[:,[i]]*dataframe.iloc[:,[j]]
+    for i in range (3,10):
+        for j in range (i+1,10):
+            mutiple = dataframe.iloc[:,[i]].values*dataframe.iloc[:,[j]].values
             df[column_list[i] +'与'+ column_list[j] +'乘积的均值'] = mutiple.mean()
             df[column_list[i] +'与'+ column_list[j] +'乘积的标准差'] = mutiple.std()
     
+    #循环构造平方项特征
+    for i in range (3,10):
+        square = dataframe.iloc[:,[i]].values * dataframe.iloc[:,[i]].values
+        df[column_list[i] + '平方项的均值'] = square.mean()
+        df[column_list[i] + '平方项的标准差'] = square.std()
+
     return df
 
 
 #处理单个训练样本
-def process_single_sample (path,train_percentage,k):
+def process_single_sample (path,train_percentage):
 
     #获取并预处理数据
-    data = preprocess(path,k)
+    data = preprocess(path,12)
     #获取该零件寿命
     work_life = data['部件工作时长'].max()
     #获取在寿命一定百分比时间的数据
@@ -205,7 +213,7 @@ def process_single_sample (path,train_percentage,k):
                     '开关1信号',
                     '开关2信号',
                     '告警信号1']:
-        dict_data=feature_project(data[item],dict_data,item,k)
+        dict_data=feature_project(data[item],dict_data,item,12)
 
     #耦合特征
     dict_data=coupled_feature(data,dict_data)
@@ -215,46 +223,119 @@ def process_single_sample (path,train_percentage,k):
 
 
 #整合处理训练集与测试集,并采用多线程
-def integrated_process (path_list,test_or_not):
+def integrated_process (cpu,path_list,test_or_not,func):
     
     #测试集处理
     if test_or_not == True:
         #测试集无需对数据进行分割处理
         train_percentage_list = [1]
         feature_list = []
+        rst = []
+        pool = Pool(cpu)
         for path in path_list:
             for train_percentage in train_percentage_list:
-                single_data = process_single_sample(path,train_percentage)
-                feature_list.append(single_data)
-        features = feature_list[0]
-        for item in feature_list:
-            features = pd.concat([features, item], axis=0)
-        columns=features.columns.tolist()
+                rst.append(pool.apply_async(func, args=(path,train_percentage)))
+        pool.close()
+        pool.join()
+        rst = [i.get() for i in rst]
+        feature_list=rst[0]
+        for item in rst[1:]:
+            feature_list = pd.concat([feature_list, item], axis=0)
+        columns=feature_list.columns.tolist()
         for col in ['train_file_name','rest_life']:
             columns.remove(col)
         columns=['train_file_name']+columns+['rest_life']
-        features['train_file_name']=features['train_file_name'].apply(lambda x:x[:-1])
-        features=features.reindex(columns=columns)
+        feature_list['train_file_name']=feature_list['train_file_name'].apply(lambda x:x[:-1])
+        feature_list=feature_list.reindex(columns=columns)
 
     #训练集处理
     if test_or_not == False:
         #训练集目的为预测剩余寿命，故将数据集分割
         train_percentage_list = [0.45,0.55,0.63,0.75,0.85]
         feature_list = []
+        rst = []
+        pool = Pool(cpu)
         for path in path_list:
             for train_percentage in train_percentage_list:
-                feature_list.append(process_single_sample(path,train_percentage))
-        features = feature_list[0]
-        for item in feature_list:
-            features = pd.concat([features, item], axis=0)
-        columns=features.columns.tolist()
+                rst.append(pool.apply_async(func, args=(path,train_percentage)))
+        pool.close()
+        pool.join()
+        rst = [i.get() for i in rst]
+        feature_list=rst[0]
+        for item in rst[1:]:
+            feature_list = pd.concat([feature_list, item], axis=0)
+        columns=feature_list.columns.tolist()
         for col in ['train_file_name','rest_life']:
             columns.remove(col)
         columns=['train_file_name']+columns+['rest_life']
-        features=features.reindex(columns=columns)
+        feature_list=feature_list.reindex(columns=columns)
     
-    return features
+    return feature_list
 
+#评价指标
+def compute_loss(target, predict):
+    temp = np.log(abs(target + 1)) - np.log(abs(predict + 1))
+    res = np.sqrt(np.dot(temp, temp) / len(temp))
+    return res
+
+#pearson系数
+def pearson(train):
+
+    pearson_list = []
+    for i in range(2,train.shape[1]-1):
+        length = len(train['rest_life'])
+        a = train.iloc[:,[i]].values.flatten()
+        b = train['rest_life'].values.flatten()
+        r,p = pearsonr(a,b) 
+        pearson_list.append(r)
+    return pearson_list
+
+
+#lgb
+def lgb_cv(train, params, fit_params,feature_names, nfold, seed,test):
+    train_pred = pd.DataFrame({
+        'true': train[ycol],
+        'pred': np.zeros(len(train))})
+    test_pred = pd.DataFrame({idx: test[idx], ycol: np.zeros(len(test))},columns=[idx,ycol])
+    kfolder = KFold(n_splits=nfold, shuffle=True, random_state=seed)
+    for fold_id, (trn_idx, val_idx) in enumerate(kfolder.split(train)):
+        print('\nFold_{fold_id} Training ================================\n'.format(fold_id = fold_id))
+        lgb_trn = lgb.Dataset(
+            data=train.iloc[trn_idx][feature_names],
+            label=train.iloc[trn_idx][ycol],
+            feature_name=feature_names)
+        lgb_val = lgb.Dataset(
+            data=train.iloc[val_idx][feature_names],
+            label=train.iloc[val_idx][ycol],
+            feature_name=feature_names)
+        lgb_reg = lgb.train(params=params, train_set=lgb_trn,
+                            num_boost_round = fit_params['num_boost_round'], verbose_eval = fit_params['verbose_eval'],
+                            early_stopping_rounds = fit_params['early_stopping_rounds'], #**fit_params,\
+                  valid_sets=[lgb_trn, lgb_val])
+        val_pred = lgb_reg.predict(
+            train.iloc[val_idx][feature_names],
+            num_iteration=lgb_reg.best_iteration)
+        
+        train_pred.loc[val_idx, 'pred'] = val_pred
+        test_pred[ycol] += (np.exp(lgb_reg.predict(test[feature_names]))-1) 
+    test_pred[ycol] = test_pred[ycol] / nfold
+    score = compute_loss(pd.Series(np.exp(train_pred['true']) - 1).apply(max, args=(0,))
+                         ,pd.Series(np.exp(train_pred['pred']) - 1).apply(max, args=(0,)))
+    print('\nCV LOSS:', score)
+    return test_pred
+
+
+# ====== lgb ======
+params_lgb = {'num_leaves': 250, 
+              'max_depth':5, 
+              'learning_rate': 0.02,
+              'objective': 'regression', 
+              'boosting': 'gbdt',
+              'verbosity': -1}
+
+fit_params_lgb = {'num_boost_round': 5000, 
+                  'verbose_eval':200,
+                  'early_stopping_rounds': 200}
 
 #主进程，调试使用
 if __name__ == '__main__':
@@ -262,25 +343,78 @@ if __name__ == '__main__':
     #获取路径集
     train_path = 'train'
     test_path = 'test1'
-    train_list = get_filelist(train_path,[])
-    test_path = get_filelist(test_path,[])
+    n=4
 
-    data = process_single_sample(train_list[0],1,6)
-    print (data)
+    train_list = get_filelist(train_path,[])
+    test_list = get_filelist(test_path,[])
+
+
+    func=process_single_sample
+    train=integrated_process(n,train_list,False,func)
+    # p_list = pearson(train)
+    # print (p_list)
+    # test =integrated_process(n,test_list,True,func)
+    # print("done.", time.time()-start)
+
+    # sub= lgb_cv(train_test.iloc[:train.shape[0]] ,params_lgb, fit_params_lgb, 
+    #             feature_name, 5,2018,train_test.iloc[train.shape[0]:])
+
+    # sub.to_csv('baseline_sub1.csv',index=False)
+    # print("process(es) done.", time.time()-start)
+    
+    # data = process_single_sample(train_list[0],1,12)
+    # for i in range(data.shape[1]):
+    #     print (data.iloc[:,[i]])
+    
+    
+##----------------------这里是用于测试的函数----------------------##
+
+    ##不同type的元件个数
+    # list_type = ['S51d', 'S26a', 'S100', 'Saa3', 'S508']
+    # num_type = [0,0,0,0,0]
+    # num_type_2 = [0,0,0,0,0]
+    # for train_path in train_list:
+    #     data = pd.read_csv(train_path)
+    #     for i in range(5):
+    #         if data['设备类型'][0] == list_type[i]:
+    #             num_type[i] = num_type[i] + 1
+    # for test_path in test_list:
+    #     data = pd.read_csv(test_path)
+    #     for i in range(5):
+    #         if data['设备类型'][0] == list_type[i]:
+    #             num_type_2[i] = num_type_2[i] + 1
+    # print (num_type)
+    # print (num_type_2)
+
+    # #训练集中type分布
+    # list_type = ['S51d', 'S26a', 'S100', 'Saa3', 'S508']
+    # for i in range(5):
+    #     exec('life_list{}=list()'.format(i))
+    # for train_path in train_list:
+    #     data = pd.read_csv(train_path)
+    #     for i in range(5):
+    #         if data['设备类型'][0] == list_type[i]:
+    #             # max_life = data['部件工作时长'].max()
+    #             exec('life_list{}.append(data.部件工作时长.max())'.format(i))
+    # for i in range(5):
+    #     exec('life_list{}.sort()'.format(i))
+    # plt.figure()
+    # for i in range(5):
+    #     # exec('plt.hist(life_list{})'.format(i))
+    #     plt.subplot(231+i)
+    #     exec('counter = len(life_list{})+1'.format(i))
+    #     exec('plt.scatter(range(1,counter),life_list{})'.format(i))
+    #     plt.title(list_type[i],fontsize = 20)
+    # plt.show()
     
     # #绘制图像观测数据分布特征
     # plt.figure()
     # for csv_path in train_list:
     #     raw_data = pd.read_csv(csv_path)
-    #     plt.scatter(raw_data['部件工作时长'],raw_data['流量信号'])
+    #     plt.scatter(raw_data['部件工作时长'],raw_data['告警信号1'])
 
     #     #窗口最大化
     #     mng = plt.get_current_fig_manager()
     #     mng.window.state('zoomed') #works fine on Windows!
 
     #     plt.show()
-
-    # train=integrated_process(train_list,False)
-    # test =integrated_process(test_list,True)
-
-    # print (train)
